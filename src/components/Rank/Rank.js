492eb7d5b4ab81956c3ef02dbbdca603e797927e
HemisCoding <117861750+HemisCoding@users.noreply.github.com>
2023-05-11 18:58:19 +0300

    Update Rank.js

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -5,11 +5,7 @@ const Rank = ({ name, entries }) => {
     return (
         <div>
             <div className='white f3 '>
-<<<<<<< HEAD
                 {`${name}, your current entry count is...`} 
-=======
-                { `${name}, your current entry count is... `}
->>>>>>> origin/main
             </div>
             <div className='white f1 '>
                 {entries}
