9d1ccb002cf1f76825b85352c906035ac7ac0634
HemisCoding <117861750+HemisCoding@users.noreply.github.com>
2023-05-13 12:08:46 +0300

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
 
     onSubmitSignIn = (event) => {
         event.preventDefault();
-        fetch("http://localhost:3000/signin", {
+        fetch("https://backend-face-detection-hemiscoding.onrender.com/signin", {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
